374083861c8721cd3da7837373b8cdb920c67300
ojasvi2828 <77545304+ojasvi2828@users.noreply.github.com>
2021-06-11 11:44:51 -0400

    Add files via upload

--- rope.js
+++ rope.js
@@ -5,7 +5,7 @@ class Rope {
         var options = {
             bodyA:body1,
             bodyB:body2,
-           pointB:{X:this.offsetX , Y:this.offsetY  }
+           pointB:{x:this.offsetX , y:this.offsetY  }
         }
         this.rope=Constraint.create(options);
     World.add(world,this.rope);
--- sketch.js
+++ sketch.js
@@ -19,8 +19,16 @@ function setup() {
 	//Create the Bodies Here.
 
  roof = new Roof();
-bob1 = new Bob(400,600,40);
-rope1= new Rope(bob1.body,roof.body,0,0);
+bob1 = new Bob(320,600,40);
+rope1= new Rope(bob1.body,roof.body,-80,0);
+bob2 = new Bob(360,600,40);
+rope2= new Rope(bob2.body,roof.body,-40,0);
+bob3 = new Bob(400,600,40);
+rope3= new Rope(bob3.body,roof.body,0,0);
+bob4 = new Bob(440,600,40);
+rope4= new Rope(bob4.body,roof.body,40,0);
+bob5 = new Bob(480,600,40);
+rope5= new Rope(bob5.body,roof.body,80,0);
 	Engine.run(engine);
   
 }
@@ -28,13 +36,25 @@ rope1= new Rope(bob1.body,roof.body,0,0);
 
 function draw() {
   rectMode(CENTER);
-  background(0);
+  background("white");
   roof.display();
   rope1.display();
   bob1.display();
+  rope2.display();
+  bob2.display();
+  rope3.display();
+  bob3.display();
+  rope4.display();
+  bob4.display();
+  rope5.display();
+  bob5.display();
   drawSprites();
  
 }
 
-
+function keyPressed(){
+  if(keyCode === UP_ARROW){
+    Matter.Body.applyForce(bob1.body,bob1.body.position,{x:-50,y:-40});
+  }
+}
 
